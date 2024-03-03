de3a1ce5e35f426b06ae30b2f4dd4b611fb920da
ranapat <ranapat@gmail.comm>
2024-03-03 09:59:08 +0100

    group similar methods

--- instancefactory/src/main/java/org/ranapat/instancefactory/Helpers.kt
+++ instancefactory/src/main/java/org/ranapat/instancefactory/Helpers.kt
@@ -5,14 +5,12 @@ import java.lang.ref.WeakReference
 inline fun <reified T> lazyInject() : Lazy<T> = lazy {
     Fi.get(T::class.java)
 }
-
 inline fun <reified T> lazyInject(vararg varargs: Any) : Lazy<T> = lazy {
     val types = varargs.map { it.javaClass }.toTypedArray()
     val values = varargs.mapIndexed { index, value -> types[index].cast(value)}.toTypedArray()
 
     InstanceFactory.get(T::class.java, types, *values)
 }
-
 inline fun <reified T> lazyWeakInject(): Lazy<WeakReference<T>> = lazy {
     WeakReference(Fi.get(T::class.java))
 }
@@ -20,14 +18,12 @@ inline fun <reified T> lazyWeakInject(): Lazy<WeakReference<T>> = lazy {
 inline fun <reified T> inject(): T {
     return Fi.get(T::class.java)
 }
-
 inline fun <reified T> inject(vararg varargs: Any): T {
     val types = varargs.map { it.javaClass }.toTypedArray()
     val values = varargs.mapIndexed { index, value -> types[index].cast(value)}.toTypedArray()
 
     return InstanceFactory.get(T::class.java, types, *values)
 }
-
 inline fun <reified T> weakInject(): WeakReference<T> {
     return WeakReference(Fi.get(T::class.java))
 }
@@ -35,14 +31,12 @@ inline fun <reified T> weakInject(): WeakReference<T> {
 inline fun <reified T> lazyGet() : Lazy<T> = lazy {
     Fi.get(T::class.java)
 }
-
 inline fun <reified T> lazyGet(vararg varargs: Any) : Lazy<T> = lazy {
     val types = varargs.map { it.javaClass }.toTypedArray()
     val values = varargs.mapIndexed { index, value -> types[index].cast(value)}.toTypedArray()
 
     InstanceFactory.get(T::class.java, types, *values)
 }
-
 inline fun <reified T> lazyWeakGet(): Lazy<WeakReference<T>> = lazy {
     WeakReference(Fi.get(T::class.java))
 }
@@ -50,14 +44,12 @@ inline fun <reified T> lazyWeakGet(): Lazy<WeakReference<T>> = lazy {
 inline fun <reified T> get(): T {
     return Fi.get(T::class.java)
 }
-
 inline fun <reified T> get(vararg varargs: Any): T {
     val types = varargs.map { it.javaClass }.toTypedArray()
     val values = varargs.mapIndexed { index, value -> types[index].cast(value)}.toTypedArray()
 
     return InstanceFactory.get(T::class.java, types, *values)
 }
-
 inline fun <reified T> weakGet(): WeakReference<T> {
     return WeakReference(Fi.get(T::class.java))
 }
