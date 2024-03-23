523757ede8441bcc669a4ad10f3496dd9c7c80f8
ranapat <ranapat@gmail.com>
2024-03-23 10:36:41 +0100

    kotlin unique helpers

--- instancefactory/src/main/java/org/ranapat/instancefactory/Helpers.kt
+++ instancefactory/src/main/java/org/ranapat/instancefactory/Helpers.kt
@@ -3,6 +3,40 @@ package org.ranapat.instancefactory
 import java.lang.ref.WeakReference
 
 /**
+ * Unique getters block start
+ */
+
+inline fun <reified T> lazyUnique() : Lazy<T> = lazy {
+    Fi.unique(T::class.java)
+}
+inline fun <reified T> lazyUnique(vararg varargs: Any) : Lazy<T> = lazy {
+    val types = varargs.map { it.javaClass }.toTypedArray()
+    val values = varargs.mapIndexed { index, value -> types[index].cast(value)}.toTypedArray()
+
+    InstanceFactory.unique(T::class.java, types, *values)
+}
+inline fun <reified T> lazyWeakUnique(): Lazy<WeakReference<T>> = lazy {
+    WeakReference(Fi.unique(T::class.java))
+}
+
+inline fun <reified T> unique(): T {
+    return Fi.unique(T::class.java)
+}
+inline fun <reified T> unique(vararg varargs: Any): T {
+    val types = varargs.map { it.javaClass }.toTypedArray()
+    val values = varargs.mapIndexed { index, value -> types[index].cast(value)}.toTypedArray()
+
+    return InstanceFactory.unique(T::class.java, types, *values)
+}
+inline fun <reified T> weakUnique(): WeakReference<T> {
+    return WeakReference(Fi.unique(T::class.java))
+}
+
+/**
+ * Unique getters block end
+ */
+
+/**
  * Default namespace injectors block start
  */
 
